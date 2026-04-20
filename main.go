e8da6ed75f60db377e00f4607c362adb1e25b09d
rassnner <ranner.j@proton.me>
2026-04-20 15:38:44 +0200

    fortschritte gemacht

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"image/color"
+	"math/rand"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
@@ -15,23 +16,44 @@ type Game struct {
 	snake     []Point
 	direction Point
 	food      Point
+	ticker int
 }
 
 func NewGame() *Game {
 	return &Game{
-		snake:     []Point{{X: 10, Y: 10}},
+		snake: []Point{{X: 10, Y: 10}, {X: 9, Y: 10}, {X: 8, Y: 10}},
 		direction: Point{X: 1, Y: 0},
 		food:      Point{X: 20, Y: 20},
 	}
 }
 
 func (g *Game) Update() error {
-	head := g.snake[0]
-	newHead := Point{
+	g.ticker++
+	switch{
+	case ebiten.IsKeyPressed(ebiten.KeyArrowUp):
+		g.direction = Point{X: 0, Y: -1}
+	case ebiten.IsKeyPressed(ebiten.KeyArrowDown):
+		g.direction = Point{X: 0, Y: 1}
+	case ebiten.IsKeyPressed(ebiten.KeyArrowLeft):
+		g.direction = Point{X: -1, Y: 0}
+	case ebiten.IsKeyPressed(ebiten.KeyArrowRight):
+		g.direction = Point{X: 1, Y: 0}
+	}
+//food: Point{X: rand.Intn(30), Y: rand.Intn(30)}
+	if g.ticker % 10 == 0{
+		head := g.snake[0]
+		newHead := Point{
 		X: head.X + g.direction.X,
 		Y: head.Y + g.direction.Y,
+		}
+
+		if newHead == g.food{
+			g.food = Point{X: rand.Intn(30), Y: rand.Intn(30)}
+			g.snake = append([]Point{newHead}, g.snake...)
+		} else{
+		g.snake = append([]Point{newHead}, g.snake[:len(g.snake)-1]...)
+		}
 	}
-	g.snake = append(g.snake, newHead)
 	return nil
 }
 
@@ -46,9 +68,11 @@ func (g *Game) Draw(screen *ebiten.Image) {
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 620, 240
+	return 600, 600
 }
 
 func main() {
+	ebiten.SetWindowSize(600,600)
+	ebiten.SetWindowTitle("Snake")
 	ebiten.RunGame(NewGame())
 }
